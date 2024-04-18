efc14c0bf4c0f70bb702662eb081f100f278b525
marie-lourdes <marielourdesleperlier64@gmail.com>
2024-04-18 15:43:04 +0200

    fetch branch testbranch

--- microservice-commandes/src/test/java/com/mcommandes/McommandesApplicationTests.java
+++ microservice-commandes/src/test/java/com/mcommandes/McommandesApplicationTests.java
@@ -1,7 +1,6 @@
 package com.mcommandes;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
