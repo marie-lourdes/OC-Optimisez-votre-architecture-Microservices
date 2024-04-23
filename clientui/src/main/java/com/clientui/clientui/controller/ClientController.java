df0ac7789ca740b72afa4e9250a720bd1aa08753
marie-lourdes <marielourdesleperlier64@gmail.com>
2024-04-23 07:51:42 +0200

    add model produits in methode and route @Get ('/')

--- clientui/src/main/java/com/clientui/clientui/controller/ClientController.java
+++ clientui/src/main/java/com/clientui/clientui/controller/ClientController.java
@@ -1,18 +1,26 @@
 package com.clientui.clientui.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.clientui.clientui.beans.ProductBean;
+import com.clientui.clientui.proxies.MicroserviceProduitsProxy;
+
 @Controller
 public class ClientController {
+   private final MicroserviceProduitsProxy produitsProxy;
 
-	  @RequestMapping("/")
+   public ClientController(MicroserviceProduitsProxy produitsProxy){
+       this.produitsProxy = produitsProxy;
+   }
 
-	  public String accueil(Model model)
-	  {
-
-	      return "Accueil";
-
-	  }
+   @RequestMapping("/")
+   public String accueil(Model model){
+       List<ProductBean> produits =  produitsProxy.listeDesProduits();
+       model.addAttribute("produits", produits);
+       return "Accueil";
+   }
 }
