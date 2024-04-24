a4282a8cecc0a34836a95b669f471f88e370e6a6
marie-lourdes <marielourdesleperlier64@gmail.com>
2024-04-24 13:04:19 +0200

    implement communication with feign Clientui and micrroservice-commande

---
+++ clientui/src/main/java/com/clientui/clientui/proxies/MicroserviceCommandeProxy.java
@@ -0,0 +1,14 @@
+package com.clientui.clientui.proxies;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.clientui.clientui.beans.CommandeBean;
+
+@FeignClient(name = "microservice-commandes", url = "localhost:9002")
+public interface MicroserviceCommandeProxy {
+
+    @PostMapping(value = "/commandes")
+    CommandeBean ajouterCommande(@RequestBody CommandeBean commande);
+}
