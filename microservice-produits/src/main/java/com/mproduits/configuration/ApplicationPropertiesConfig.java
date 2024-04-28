ef0873d93a77b1288a70dcc463ce83cfe3743482
marie-lourdes <marielourdesleperlier64@gmail.com>
2024-04-28 17:19:43 +0200

    test refresh propertie limitproduit 3

--- microservice-produits/src/main/java/com/mproduits/configuration/ApplicationPropertiesConfig.java
+++ microservice-produits/src/main/java/com/mproduits/configuration/ApplicationPropertiesConfig.java
@@ -1,13 +1,14 @@
 package com.mproduits.configuration;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Component;
 
 /*@ConfigurationProperties("mes-configs")  :
  *  précise que cette classe de configuration va récupérer des propriétés dans application.properties dont le préfixe est  mes-configs.*/
 @Component
 @ConfigurationProperties("mes-configs")
-@RefreshScope
+//@RefreshScope
 public class ApplicationPropertiesConfig {
 
 	  private int limitDeProduits;
