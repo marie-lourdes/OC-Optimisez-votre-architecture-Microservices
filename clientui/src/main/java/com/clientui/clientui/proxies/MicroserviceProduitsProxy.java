7171548fe4ae09e4564e2f0b7518cc4820d3df14
marie-lourdes <marielourdesleperlier64@gmail.com>
2024-04-30 23:10:06 +0200

    add comments in MicroserviceProduitsProxy

--- clientui/src/main/java/com/clientui/clientui/proxies/MicroserviceProduitsProxy.java
+++ clientui/src/main/java/com/clientui/clientui/proxies/MicroserviceProduitsProxy.java
@@ -2,6 +2,7 @@ package com.clientui.clientui.proxies;
 
 import java.util.List;
 
+
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +10,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import com.clientui.clientui.beans.ProductBean;
 
+/*-url = "localhost:9001" desactivé car  Ribbon est lié a eureka , 
+ * la liste des URL configuré : microservice-produits.ribbon.listOfServers=localhost:9001,localhost:9011 (microservice-clientui.propertie du config server repo distant)
+ * n est pas necessaire, ribbon recuperes les instances du microservice enregistré dans eureka server
+ * 
+ * -connexion avec le font 8080 n affiche pas le template 
+mais sera replacé par api gateway et zuul server qui fera la requete http vers eureka server nativement relié ensemble zuul et eureka,
+et chargera les Url des microservices*/
 @FeignClient(name = "microservice-produits"/*, url = "localhost:9001"*/)
 @RibbonClient(name = "microservice-produits")
 public interface MicroserviceProduitsProxy {
