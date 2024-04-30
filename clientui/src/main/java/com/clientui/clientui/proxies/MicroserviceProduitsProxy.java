8c380d7d6cd978247e7ebd6f7e040df55405fe70
marie-lourdes <marielourdesleperlier64@gmail.com>
2024-04-30 17:30:37 +0200

    add Ribbon to microservice-clientui

--- clientui/src/main/java/com/clientui/clientui/proxies/MicroserviceProduitsProxy.java
+++ clientui/src/main/java/com/clientui/clientui/proxies/MicroserviceProduitsProxy.java
@@ -2,6 +2,7 @@ package com.clientui.clientui.proxies;
 
 import java.util.List;
 
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
