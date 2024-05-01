5eebb0fbded1ba2497c1cf2096fa82f4eaa8400d
marie-lourdes <marielourdesleperlier64@gmail.com>
2024-05-01 10:24:56 +0200

    add eureka discovery client to all microservice and run allmicroservice  to register intances in eureka server

--- microservice-paiement/src/main/java/com/mpaiement/MpaiementApplication.java
+++ microservice-paiement/src/main/java/com/mpaiement/MpaiementApplication.java
@@ -2,10 +2,13 @@ package com.mpaiement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+//EnableFeignClients effectue des requete http client au microservice-commande pour preciser que la commande est payée et Feign client scanne les classes de microservice-paiement
 @SpringBootApplication
 @EnableFeignClients("com.mpaiement")
+@EnableDiscoveryClient
 public class MpaiementApplication {
 
 	public static void main(String[] args) {
