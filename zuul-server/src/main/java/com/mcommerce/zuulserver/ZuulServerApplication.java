7a750b4a7aca68616d8c1e7699b23162e922ca0b
marie-lourdes <marielourdesleperlier64@gmail.com>
2024-05-01 11:22:20 +0200

    add spring-cloud-starter-netflix-zuul in microservice zuul-server

--- zuul-server/src/main/java/com/mcommerce/zuulserver/ZuulServerApplication.java
+++ zuul-server/src/main/java/com/mcommerce/zuulserver/ZuulServerApplication.java
@@ -2,8 +2,11 @@ package com.mcommerce.zuulserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableZuulProxy
+@EnableDiscoveryClient
 public class ZuulServerApplication {
 
 	public static void main(String[] args) {
