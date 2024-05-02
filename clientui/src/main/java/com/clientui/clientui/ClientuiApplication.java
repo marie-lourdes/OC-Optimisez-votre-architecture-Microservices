2cc94c99dd4d5af7ab9d0ab8afe91cd9f342540e
marie-lourdes <marielourdesleperlier64@gmail.com>
2024-05-02 13:32:30 +0200

    remove spring-cloud-starter-netflix-zuul in  pom.xml of microservice-clientui

--- clientui/src/main/java/com/clientui/clientui/ClientuiApplication.java
+++ clientui/src/main/java/com/clientui/clientui/ClientuiApplication.java
@@ -3,7 +3,6 @@ package com.clientui.clientui;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
