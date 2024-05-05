dc3d459d6a94492977cbfe2b5b349c3dd657361e
marie-lourdes <marielourdesleperlier64@gmail.com>
2024-05-05 17:14:54 +0200

    create class configuration  RouteLocatorConfig.java with bean DiscoveryClientRouteDefinitionLocator locator

--- api-gateway/src/main/java/com/mcommerce/apigateway/config/RouteLocatorConfig.java
+++ api-gateway/src/main/java/com/mcommerce/apigateway/config/RouteLocatorConfig.java
@@ -1,9 +1,21 @@
 package com.mcommerce.apigateway.config;
 
-/*@Configuration
-public class RouteLocatorConfig {
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+public class RouteLocatorConfig {
+	
 	@Bean
+	public  DiscoveryClientRouteDefinitionLocator locator(ReactiveDiscoveryClient dc, DiscoveryLocatorProperties dlp) {
+		return new DiscoveryClientRouteDefinitionLocator(dc,dlp);
+	}
+	
+
+/*	@Bean
 	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
 	    return builder.routes()
 	        .route(p -> p
@@ -11,5 +23,5 @@ public class RouteLocatorConfig {
 	            .filters(f -> f.addRequestHeader("Hello", "World"))
 	            .uri("http://httpbin.org:80"))
 	        .build();
-	}
-}*/
+	}*/
+}
